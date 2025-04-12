8ca25b97a843dc78937e169aa3506692620486f1
Raphael Schuhmann <raphael221@outlook.de>
2025-04-12 23:30:06 +0200

    Added error handling to adminAuthMiddleware.js

--- src/middleware/adminAuthMiddleware.js
+++ src/middleware/adminAuthMiddleware.js
@@ -1,12 +1,24 @@
 const User = require('../models/User');
+const logger = require('../config/logger');
 
 const adminAuthMiddleware = async (req, res, next) => {
-    const user = await User.findOne({userId: req.user});
+    try {
+        if (!req.user) {
+            return res.status(401).json({error: "You are not authorized to perform this action!"});
+        }
 
-    if (user.role !== 'admin') {
-        return res.status(401).json({error: "You are not authorized to perform this action!"});
+        const user = await User.findOne({userId: req.user});
+        if (!user) return res.status(404).json({error: "User doesn't exists!"});
+
+        if (user.role !== 'admin') {
+            return res.status(401).json({error: "You are not authorized to perform this action!"});
+        }
+
+        next();
+    } catch (err) {
+        logger.error(err);
+        return res.status(500).json({error: "There was an internal server error! Please try again! If this error keeps occurring, please contact the developer!"});
     }
-    next();
 };
 
 module.exports = adminAuthMiddleware;
